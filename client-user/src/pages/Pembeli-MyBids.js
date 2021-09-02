f611c3d7e809aa2b66efb538903ff03ceb00fcce
anggidast <adastariana@gmail.com>
2021-09-02 23:31:58 +0700

    fix post new product

--- client-user/src/pages/Pembeli-MyBids.js
+++ client-user/src/pages/Pembeli-MyBids.js
@@ -237,8 +237,19 @@ function PembeliMyBids() {
                                             onClick={() => triggerEditModal(bid)}
                                             className="text-gray-500 cursor-pointer hover:text-gray-400 font-medium"
                                           >
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                            <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              class="h-6 w-6"
+                                              fill="none"
+                                              viewBox="0 0 24 24"
+                                              stroke="currentColor"
+                                            >
+                                              <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="1.5"
+                                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                              />
                                             </svg>
                                             {/* Edit */}
                                           </a>
@@ -246,8 +257,19 @@ function PembeliMyBids() {
                                             onClick={() => triggerDeleteModal(bid.id)}
                                             className="text-red-600 hover:text-red-400 font-medium cursor-pointer"
                                           >
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              class="h-6 w-6"
+                                              fill="none"
+                                              viewBox="0 0 24 24"
+                                              stroke="currentColor"
+                                            >
+                                              <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="1.5"
+                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                              />
                                             </svg>
                                             {/* Hapus */}
                                           </a>
